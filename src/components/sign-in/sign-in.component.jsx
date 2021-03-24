d31a77e908b061aaa866655185b0ec50670aeaf0
Danny Mercado <damercado87@gmail.com>
2021-03-24 15:30:52 -0700

    stylized button and added google auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -48,9 +48,10 @@ class SignIn extends React.Component {
                         value={this.state.password}
                         handleChange={this.handleChange}
                         required />
-
+                    <div className='button'>
                     <CustomButton type="submit"> Sign In </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> {''} Sign In With Google{''}</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn> {''} Sign In With Google{''}</CustomButton>
+                    </div>
                 </form>
             </div>
         )
